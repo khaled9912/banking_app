75ea47a1a99c6a7a9e8bf1c8b1985b50b57402c0
khaled9912 <khaledmourad23345@gmail.com>
2024-08-06 23:53:05 +0300

    add right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: "Khaled" };
+  const loggedIn = { firstName: "Khaled", lastName: "Mourad", email: "demo@gmail.com" };
 
   return (
     <section className="home">
@@ -23,8 +24,14 @@ const Home = () => {
           />
 
         </header>
-      </div>
 
+        RECENT TRANSACTIONS
+      </div>
+        <RightSidebar 
+          user={loggedIn}
+          transactions={[]}
+          banks={[]}
+        />
     </section>
   )
 }
